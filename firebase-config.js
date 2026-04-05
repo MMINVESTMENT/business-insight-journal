8b9eb486702498b4743eeda850223bfb85eefea0
MMINVESTMENT <120617571+MMINVESTMENT@users.noreply.github.com>
2026-04-05 20:39:34 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js (Firebase v8 全局风格)
 var firebaseConfig = {
   apiKey: "AIzaSyDdM_FPc6yRG-MSK4VNFeG9Gpeq1SDWjU",
   authDomain: "journal-recoding-website.firebaseapp.com",
@@ -8,8 +7,6 @@ var firebaseConfig = {
   appId: "1:372413301932:web:0d28e9ff98a65d58300b09",
   measurementId: "G-DGJP4KEXVQ"
 };
-// 初始化
 firebase.initializeApp(firebaseConfig);
-// 可选：方便后续直接用
 var auth = firebase.auth();
 var db = firebase.firestore();
